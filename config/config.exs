6ff683231ff13951c69a39ccf99c3c3601dd2016
Roman Pushkov <pushkovroman@me.com>
2018-10-28 19:22:09 +0300

    update config

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,8 @@
 use Mix.Config
 
 config :kv, storage: :storage
+config :kv, storage_file: 'storage.tab'
+config :kv, persistence_interval: 1000
 config :kv, port: 7777
 
 import_config "#{Mix.env()}.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,6 @@
 use Mix.Config
 
 config :kv, storage: :test
+config :kv, storage_file: 'test.tab'
+config :kv, persistence_interval: :infinity
 config :kv, port: 7778
